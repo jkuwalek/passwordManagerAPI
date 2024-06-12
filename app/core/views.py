e90f84c9f5b5a5a99f942a5243f696503f6a83f0
Jakub Kuwałek <jacob@shieldia.net>
2024-06-12 20:45:59 +0200

    minor lint fix

--- app/core/views.py
+++ app/core/views.py
@@ -4,6 +4,7 @@ Core views for app
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+
 @api_view(['GET'])
 def health_check(request):
     """Returns successful response"""
